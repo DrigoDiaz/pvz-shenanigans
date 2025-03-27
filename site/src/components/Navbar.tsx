6c010c62608dd26467ded2fa4a12bab8f07bb7fa
k1tk4t5 <akemi90638@gmail.com>
2025-03-26 21:38:21 -0700

    Making navbar responsive for mobile

--- site/src/components/Navbar.tsx
+++ site/src/components/Navbar.tsx
@@ -4,27 +4,50 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { useTheme } from '@mui/material';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import NavbarMenu from './NavbarMenu';
 
 export default function Navbar() {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
     <>
       <CssBaseline />
-      <AppBar component="nav" style={{backgroundColor: "#2c5c3c"}}>
-      <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'></link>
+      <AppBar component="nav" style={{ backgroundColor: "#2c5c3c" }}>
+        <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'></link>
         <Toolbar>
-          <Typography
-            variant="h6"
-            sx={{ flexGrow: 0, display: { xs: 'none', sm: 'block', color: 'white'}, fontFamily: "Montserrat"}}
-          >
-            Plants vs Zombies
-          </Typography>
-          <Box sx={ {flexGrow: 1 } }></Box>
-          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-            <Button key="Home" sx={{ color: 'white', fontFamily: "Montserrat" }} href='/'>Home</Button>
-            <Button key="About" sx={{ color: 'white', fontFamily: "Montserrat" }} href='/about'>About</Button>
-          </Box>
+          {isSmallScreen ? (
+            // Content for small screens
+            <>
+              <Typography
+                variant="body1"
+                sx={{ flexGrow: 0, display: { sm: 'block', color: 'white' }, fontFamily: "Montserrat" }}
+              >
+                Plants vs Zombies
+              </Typography>
+              <Box sx={{ flexGrow: 1 }}></Box>
+              <NavbarMenu></NavbarMenu>
+            </>
+          ) : (
+            // Content for large screens
+            <>
+              <Typography
+                variant="h6"
+                sx={{ flexGrow: 0, display: { xs: 'none', sm: 'block', color: 'white' }, fontFamily: "Montserrat" }}
+              >
+                Plants vs Zombies
+              </Typography>
+              <Box sx={{ flexGrow: 1 }}></Box>
+              <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                <Button key="Home" sx={{ color: 'white', fontFamily: "Montserrat" }} href='/'>Home</Button>
+                <Button key="About" sx={{ color: 'white', fontFamily: "Montserrat" }} href='/about'>About</Button>
+              </Box>
+            </>
+          )}
         </Toolbar>
       </AppBar>
-      </>
+    </>
   );
 }
